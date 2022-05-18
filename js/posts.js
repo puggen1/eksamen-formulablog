ad13b2d0017379340ee0f28741f79c43d649b0e1
puggen1 <43915072+puggen1@users.noreply.github.com>
2022-05-18 18:13:55 +0200

    added the filter function with tags, not finished

--- js/posts.js
+++ js/posts.js
@@ -2,39 +2,83 @@ import { getData } from "./getData.js";
 import {getImage} from "./getImage.js";
 import {createTag} from "./createTag.js";
 import {createDate} from "./createDate.js";
+let url = window.location.search;
+let urlParams = new URLSearchParams(url);
+let tagId = urlParams.get("tagId");
 let api = "https://bendik.one/www/eksamenfed/wp-json/wp/v2/posts?per_page=20&_embed";
+let posts = [];
+let filteredPosts = [];
+//fetching all posts
+posts = await getData(api);
+//post placement
 let postSection = document.querySelector("#allPosts");
 let html="";
 let singlePost = "";
 let x = 10;
-async function showPosts(url){
-    let posts = await getData(url);
-    for (let i = 0; i < x; i++){
-        if(i < posts.length){
-            let formatedDate = createDate(posts[i].date)
-            let ImageArray = await getImage(posts[i].featured_media);
-            let tags = "";
-            for(let tag of posts[i]._embedded['wp:term'][1]){
-                let singleTag = await createTag(tag);
-                tags += singleTag;
-            }
-            singlePost = `<div class="post"><img src="${ImageArray[0]}" alt="${ImageArray[1]}">
-            <h3>${posts[i].title.rendered}</h3>
-            <p class="dateAndBy">by ${posts[i]._embedded.author[0].name}, on the ${formatedDate}</p>
-            <div class="excerpt">${posts[i].excerpt.rendered}</div>
-            <a class="blueA" href="singlepost.html?id=${posts[i].id}">Read More</a>
-            <div class="tagDiv">
-           ${tags}
-           </div>
-            </div>`;
-        }
-        else{
-            break
-        }
-        html += singlePost;
-    }
-    postSection.innerHTML = html;
-    x += 10;
+if(!tagId){
+    console.log("no tags");
+    showPosts(posts);
 }
-showPosts(api);
+else{
+   filterPost("tag", tagId);
+}
 
+
+//filter function
+
+function filterPost(method, filterId){
+    if(method == "tag"){
+        filteredPosts = posts.filter(filterTag, tagId)
+        console.log(filteredPosts)
+        }
+    else{
+        console.log("placeholder");
+    }
+
+    showPosts(filteredPosts);
+}
+
+//diffrent filter functions
+
+function filterTag(event){
+        for(let tag of event.tags){
+            if(this == tag){
+                console.log(event.tags + "inneholder taggen")
+                return true
+    
+            }
+            else{
+                return false
+            }
+        }
+    }
+//show posts, no matter filter
+    async function showPosts(postList){
+        for (let i = 0; i < x; i++){
+            if(i < postList.length){
+                let formatedDate = createDate(posts[i].date)
+                let ImageArray = await getImage(postList[i].featured_media);
+                let tags = "";
+                for(let tag of postList[i]._embedded['wp:term'][1]){
+                    let singleTag = await createTag(tag);
+                    tags += singleTag;
+                }
+                singlePost = `<div class="post"><img src="${ImageArray[0]}" alt="${ImageArray[1]}">
+                <h3>${postList[i].title.rendered}</h3>
+                <p class="dateAndBy">by ${postList[i]._embedded.author[0].name}, on the ${formatedDate}</p>
+                <div class="excerpt">${postList[i].excerpt.rendered}</div>
+                <a class="blueA" href="singlepost.html?id=${postList[i].id}">Read More</a>
+                <div class="tagDiv">
+               ${tags}
+               </div>
+                </div>`;
+            }
+            else{
+                break
+            }
+            html += singlePost;
+        }
+        postSection.innerHTML = html;
+        x += 10;
+       
+    }
--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ import {getData} from "./getData.js";
 import {getImage} from "./getImage.js";
 import {createTag} from "./createTag.js";
 import {carousel} from "./carousel.js";
+import { createDate } from "./createDate.js";
 let featuredPosts = document.querySelector("#mainPagePosts");
 export let api = "https://www.bendik.one/www/eksamenfed/wp-json/wp/v2/posts?_embed";
 let featuredMediaLibrary = "https://www.bendik.one/www/eksamenfed/wp-json/wp/v2/media/";
@@ -18,6 +19,7 @@ async function showData(data){
     let post = "";
     for(let posts of result){
         let tags = ""
+        let formatedDate = createDate(posts.date)
         for(let tag of posts._embedded['wp:term'][1]){
             let singleTag = await createTag(tag);
             tags += singleTag;
@@ -25,7 +27,7 @@ async function showData(data){
         let imageArray = await getImage(posts.featured_media);
         post = `<div class="post"><img src="${imageArray[0]}" alt="${imageArray[1]}">
                 <h3>${posts.title.rendered}</h3>
-                <p class="dateAndBy"> ${posts.date} by ${posts._embedded.author[0].name} </p>
+                <p class="dateAndBy"> ${formatedDate} by ${posts._embedded.author[0].name} </p>
                 <div class="excerpt">${posts.excerpt.rendered}</div>
                 <a class="blueA" href="singlepost.html?id=${posts.id}">Read more</a>
                 <div class="tagDiv">
