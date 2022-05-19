2418c8978baf8f542d0d92eea3a08081e0712f99
puggen1 <43915072+puggen1@users.noreply.github.com>
2022-05-19 11:49:42 +0200

    added eventlistener to btns on posts
    also have some tags as filter on all posts,

--- js/createTag.js
+++ js/createTag.js
@@ -1,3 +1,12 @@
-export async function createTag(tag){
-return `<a class="tag" href="posts.html?tagId=${tag.id}">${tag.name}</a>`
+//since i dont need <a> on the all post page, it changes to button there, so it will use filter directly and not fetch everything by reloading the page. 
+export async function createTag(tag, type="a"){
+    if(type == "button"){
+        return `<button class="tag" id="${tag.id}">${tag.name}</button>`
+    }
+    else if(type == "a"){
+        return `<a class="tag" href="posts.html?tagId=${tag.id}">${tag.name}</a>`
+    }
+    else{
+        return `<p>something went wrong<p>`
+    }
 }
--- js/posts.js
+++ js/posts.js
@@ -6,16 +6,19 @@ let url = window.location.search;
 let urlParams = new URLSearchParams(url);
 let tagId = urlParams.get("tagId");
 let api = "https://bendik.one/www/eksamenfed/wp-json/wp/v2/posts?per_page=20&_embed";
+let tagsApi = "https://bendik.one/www/eksamenfed/wp-json/wp/v2/tags?per_page=20";
+
 let posts = [];
+let tags = [];
 let filteredPosts = [];
 //fetching all posts
 posts = await getData(api);
+//tags for filters
+tags = await getData(tagsApi);
 //post placement
 let postSection = document.querySelector("#allPosts");
-let html="";
 let singlePost = "";
 let x = 7;
-let y = 0;
 let showMoreBtn = document.querySelector("#bottom");
 showMoreBtn.addEventListener("click", showMore)
 if(!tagId){
@@ -26,12 +29,24 @@ else{
    filterPost("tag", tagId);
 }
 
-
+//tag buttons
+let filterDiv = document.querySelector(".filter");
+let htmlTags = ""
+for(let tag of tags){
+    htmlTags += `<button class="tag" id="${tag.id}">${tag.name}</button>`;
+    filterDiv.innerHTML = htmlTags;
+}
+let tagBtns = document.querySelectorAll(".filter button");
+for(let btn of tagBtns){
+    btn.addEventListener("click", function (){
+        filterPost("tag", event.target.id);
+    })
+}
 //filter function
 
 function filterPost(method, filterId){
     if(method == "tag"){
-        filteredPosts = posts.filter(filterTag, tagId)
+        filteredPosts = posts.filter(filterTag, filterId)
         console.log(filteredPosts)
         }
     else{
@@ -56,13 +71,14 @@ function filterTag(event){
 
 //show posts, no matter filter
     async function showPosts(postList){
-        for (let i = y; i < x; i++){
+        let html = ""
+        for (let i = 0; i < x; i++){
             if(i < postList.length){
                 let formatedDate = createDate(posts[i].date)
                 let ImageArray = await getImage(postList[i].featured_media);
                 let tags = "";
                 for(let tag of postList[i]._embedded['wp:term'][1]){
-                    let singleTag = await createTag(tag);
+                    let singleTag = await createTag(tag, "button");
                     tags += singleTag;
                 }
                 singlePost = `<div class="post"><img src="${ImageArray[0]}" alt="${ImageArray[1]}">
@@ -81,7 +97,12 @@ function filterTag(event){
             html += singlePost;
         }
         postSection.innerHTML = html;
-        y = x;
+        let tagBtnsOnPost = document.querySelectorAll(".post button");
+        for(let tag of tagBtnsOnPost){
+            tag.addEventListener("click",function (){
+                filterPost("tag", event.target.id);
+            });
+        }
         x += 7;
        
     }
