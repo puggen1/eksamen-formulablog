a252c44a0b9fd52530f312b570fce314f36f3497
puggen1 <43915072+puggen1@users.noreply.github.com>
2022-05-30 16:51:06 +0200

    comments and margin

--- js/singlePost.js
+++ js/singlePost.js
@@ -20,6 +20,7 @@ let title = document.querySelector("title");
 let header = document.querySelector("h1");
 let metaDesc = document.querySelector(`meta[name="description"]`);
 let bigPictures;
+let imgHtml = "";
 let images;
 if (!postId) {
   window.location.replace("/index.html");
@@ -29,19 +30,13 @@ let api =
   `https://www.bendik.one/www/eksamenfed/wp-json/wp/v2/posts/` +
   postId +
   "?_embed";
-
-/*fetch(api)
-.then(result => result.json())
-.then(formatedResult => showPost(formatedResult));
-*/
 async function showPost(api) {
   let apiPost = await getData(api);
   let tags = "";
   let singleTag = "";
+
   let pictureTag = await getImage(apiPost);
-  console.log(pictureTag);
   if (!apiPost._embedded["wp:term"][1][0]) {
-    console.log("no tags");
   } else {
     for (let tag of apiPost._embedded["wp:term"][1]) {
       singleTag = await createTag(tag);
@@ -62,15 +57,19 @@ async function showPost(api) {
    <div class="tags"> ${tags}</div>
     </div>
     <a class="backBtn" href="${urlBack}">Back</a>`;
-
+  //remove the fillerpost class that have loading animation
   postLocation.classList.remove("fillerPost");
+  //making the first p preface, and adds style/id to it.
   let preface = document.querySelector("#post p");
   preface.id = "preface";
-  //big images
 
+  //i have to choose the images.
   images = document.querySelectorAll("#post img");
+
+  //the location i want the "hidden images" to be
   let bottomOfPage = document.querySelector(".bigImgPlace");
-  let imgHtml = "";
+
+  //this for loop does many things: adds icon for big picture mode, also add class to images,
   for (let i = 0; i < images.length; i++) {
     //adds icon under image
     images[i].insertAdjacentHTML(
@@ -78,12 +77,14 @@ async function showPost(api) {
       `<div class="img${i}"><i class="fas fa-expand"></i><p> click the image to make bigger</p></div>`
     );
     images[i].classList.add(`img${i}`);
+
+    //gets the content that will go in hidden images
     let imgresponse = showBigImage(images, i);
     imgHtml += imgresponse;
-    bottomOfPage.innerHTML = imgHtml;
-    //this will show big picture
+
+    //gets the icon that was added below images.
     let fullScreenIcon = document.querySelector(`div.img${i} i`);
-    //under here is a bit repetetive,
+    //under here is a bit repetetive, but it ads  eventlistener to the images and the icons so they trigger the imgEvent function that shows the big picture
 
     fullScreenIcon.addEventListener("click", function () {
       imgEvent(i);
@@ -92,20 +93,22 @@ async function showPost(api) {
       imgEvent(i);
     });
   }
+  //
+  bottomOfPage.innerHTML = imgHtml;
 
+  //adds the "go back function to all big pitures"
   bigPictures = document.querySelectorAll(".fullScreen");
-  //reset i for this test
 
   for (let pictureBox of bigPictures) {
     pictureBox.addEventListener("click", function () {
       //this if statement will check if you are pressing the img, if you are, it will not hide the image
       if (event.target.nodeName == "DIV" || event.target.nodeName == "I") {
         pictureBox.style.display = "none";
-        console.log(event.target.nodeName);
       }
     });
   }
 }
+//runs the whole function to show the page and images
 showPost(api);
 
 function imgEvent(number) {
